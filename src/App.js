47c0652759ec394d20d3012dc6fa06b17f805a06
DEIVA SIVAM <a.s.deiva@gmail.com>
2023-02-08 15:49:34 +0530

    quiz app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import questions from "./components/questions";
+import { useState } from "react";
+import React from "react";
 
 function App() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [results, setResults] = useState(false);
+  const replay = () => {
+    setQuestionIndex(0);
+    setScore(0);
+    setResults(false);
+  };
+  const present_question = questions[questionIndex];
+  const get_Choice = (index) => {
+    if (present_question.answer === index) {
+      setScore(score + 1);
+    }
+    const next_question = questionIndex + 1;
+    if (next_question < questions.length) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      setResults(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz_box">
+      
+      {results ? (
+        <>
+        <div className="resultDiv">
+          <h1>Your Score : {score}</h1>
+          <button className="againBtn" onClick={replay}>
+            Play Again
+          </button>
+          </div>
+        </>
+      ) : ( 
+        <div className="quiz_question">
+          <div className="qustions">{present_question.question}</div>
+          
+
+          <div className="quiz_box_options"></div>
+          <ul className="quiz_ul">
+            {present_question.options.map((option, i) => {
+              return (
+                <li className="quiz_li" onClick={() => get_Choice(i)}>
+                  {option}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-
+function car({ make, model }) {
+  return (
+    <h1>
+      {make} {model}
+    </h1>
+  );
+}
 export default App;
---
+++ src/components/questions.js
@@ -0,0 +1,53 @@
+export default[
+    {
+        question : "What is the correct command to create a new React project?",
+        options : ["npx create-react-app myReactApp","npx create-react-app","npm create-react-app","npm create-react-app myReactApp"],
+        answer : 0,
+    },
+    {
+        question : "What does myReactApp refer to in the following command?",
+        options : ["The directory to create the new app in","A reference to an existing app","The name you want to use for the new app","The type of app to create"],
+        answer : 2,
+    },
+    {
+        question : "What command is used to start the React local development server?",
+        options : ["npm start","npm build","npm serve","npm run dev"],
+        answer : 0,
+    },
+    {
+        question : "What is the default local host port that a React development server uses?",
+        options : ["3000","8080","5000","3500"],
+        answer : 0,
+    },
+    {
+        question : "To develop and run React code, Node.js is required.",
+        options : ["True","False"],
+        answer : 0,
+    },
+    {
+        question : "Which keyword creates a constant in JavaScript?",
+        options : ["const","var","let","constant"],
+        answer : 0,
+    },
+    {
+        question : "When rendering a list using the JavaScript map() method, what is required for each element rendered?",
+        options : ["index","id","data","key"],
+        answer : 3,
+    },
+    {
+        question : "What tool does React use to compile JSX?",
+        options : ["ReactDOM","JSX Compiler","React Router","Babel"],
+        answer : 3,
+    },
+    {
+        question : "What is the correct syntax to import a Component from React?",
+        options : ["import { Component } from 'react'","import Component from 'react'","import React.Component from 'react'","import [ Component ] from 'react'"],
+        answer : 0,
+    },
+    {
+        question : "How can you combine the following arrays using the spread operator? const array1 = [1, 2, 3]; const array2 = [4, 5, 6];",
+        options : ["const combined = ...array1 + ...array2;","const combined = [...array1, ...array2];","const combined = [array1, array2];","const combined = array1 + array2;"],
+        answer : 1,
+    }
+   ]
+
